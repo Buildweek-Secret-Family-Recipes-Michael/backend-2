b7dd26c6ff84b1621c1c00662cfe4421dadb12ad
Michelle <Michelle.Tinajero@outlook.com>
2020-09-21 21:08:21 -0500

    feat: migration
    migration but still need to do join table

--- index.ts
+++ index.ts
@@ -1,22 +1,26 @@
 import express from 'express'
 import cors from 'cors'
 import helmet from 'helmet'
+import "dotenv"
 
 //import routers here
 
 const server = express()
-const PORT = process.env.PORT || 8000
+// const PORT = process.env.PORT || 8000
 
 server.use(express.json())
 server.use(helmet())
 server.use(cors())
 //userouters
 
-server.listen(PORT, () => {
-  console.log(`\n=== Server listening on port ${PORT} ===\n`);
+
+server.get('/', (req, res) => {
+  res.json({ message: "Hello World "}) 
+})
+
+
+server.listen(process.env.PORT, () => {
+  console.log(`\n=== Server listening on port ${process.env.PORT} ===\n`)
 });
 
 
-server.get('/', (req, res) => {
-    res.json({ message: "Hello World "})  //testing out that insomnia works
-})
