bbb6edd8ba2a6025af4ccca07eb178813f19ba0b
Michelle <Michelle.Tinajero@outlook.com>
2020-09-28 16:57:57 -0500

    Well, it deployed

--- src/__tests__/user.spec.ts
+++ src/__tests__/user.spec.ts
@@ -20,37 +20,6 @@ describe('end point tests mvp', function() {
                         expect(res.body.username).toBe(res.body.username)
                     })
             })
-        
-        // #2 UMMMMM WHAT?? I AM LITERALLY SO CONFUSED WHAT DO YOU MEANNNNN
-        // it('registers a new user', async() => {
-        //     await supertest(server)
-        //             .post('/api/auth/register')
-        //             .send({ username: "michelle1" , password: "michelle1" })
-        //             .then(res => {
-        //               expect(res.statusCode).toBe(201);
-        //               expect(res.body).toStrictEqual({
-        //                 message: "The user has been created.",
-        //                 })
-        //             })
-        // })
-            
-        // //login i guess im literally so fustrated with this testing stuff man
-        // it('logs you in', async() => {
-        //     await supertest(server)
-        //             .post('/api/auth/login')
-        //             .send({ username: 'michelle1', password: 'michelle1' })
-        //             .then(res => {
-        //                 expect(res.status).toBe(200)
-        //             })
-        //         })
-                
-        //         //atleast this one works, thats all i care about now
-        //         it('gives a welcome message', async() => {
-        //             await supertest(server).post('/api/auth/login')
-        //             .send({username: 'michelle9', password: 'michelle9'})
-        //             .then(res => {
-        //                     expect(res.body).toEqual({message: `WELCOME michelle9!!!!!!!!!!!`})
-                // })
-        // })           
+                 
     })
 })
--- src/routers/recipes-router.js
+++ src/routers/recipes-router.js
@@ -1,4 +1,4 @@
-"use strict";
+// "use strict";
 // import express from 'express'
 // import * as recipeModel from  "../models/recipe-model"
 // import uuid from "uuid-1345";
