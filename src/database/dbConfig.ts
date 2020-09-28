2cd5ee1791106b9016be95f24099749f3ff2de14
Michelle <Michelle.Tinajero@outlook.com>
2020-09-28 14:10:02 -0500

    deployment changes

--- src/database/dbConfig.ts
+++ src/database/dbConfig.ts
@@ -1,8 +1,7 @@
-import dotenv from "dotenv"
-dotenv.config()
-
 const knex = require('knex');
 
 const knexConfig = require("../../knexfile");
 
-export const db = knex(knexConfig.process.env.NODE_ENV);
+const dbNode = process.env.NODE_ENV || "development"
+
+export const db = knex(knexConfig[dbNode])
